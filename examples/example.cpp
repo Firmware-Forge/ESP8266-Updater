ce2e4a9ba723896028d4ecc8c526766529253a8d
Patrick Shinn <shinn16@marshall.edu>
2020-01-12 13:13:05 -0500

    doc updates

--- examples/example.cpp
+++ examples/example.cpp
@@ -4,30 +4,29 @@
 const char* ssid = "SSID";
 const char* password = "PASSWORD";
 const String user_token = "USER_TOKEN";
-const String device_token = "DEVICE_TOKEN";
+const String user_secret = "USER_SECRET";
 
-FFUpdates updater(user_token, device_token);  // instantiate the updater
-// FFUpdates updater(user_token, device_token, true); // enables debugging, this should not be enabled for production use.
+FFUpdates updater(user_token, user_secret);  // instantiate the updater
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println();
-  Serial.print("connecting to ");
-  Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+    Serial.begin(9600);
+    Serial.println();
+    Serial.print("connecting to ");
+    Serial.println(ssid);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+    Serial.println("WiFi connected");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  Serial.println("new lib");
-  updater.update(); // check for an update, apply it if one exists.
-  delay(5000);
+    Serial.println("Checking for an update");
+    updater.update(); // check for an update, apply it if one exists.
+    delay(5000);
 }
